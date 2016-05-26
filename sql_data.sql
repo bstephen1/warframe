6ae0ac8f946dc0b1d737749b7b37687560bf0ea4
bstephen1 <bstep1@umbc.edu>
2016-05-26 14:20:27 -0400

    added ash drops

---
+++ sql_data.sql
@@ -0,0 +1,5 @@
+
+insert into parts values('ash blueprint', 50, 'high');
+insert into parts values('ash chassis', 20, 'low');
+insert into parts values('ash helmet', 30, 'low');
+insert into parts values('ash systems', 50, 'high');

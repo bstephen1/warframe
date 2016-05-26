12e0f4bdd0567e21dd7eb526e6c466f65bb9ddce
bstephen1 <bstep1@umbc.edu>
2016-05-26 14:12:15 -0400

    script to create mysql database
    creates the tables only

---
+++ sql_tables.sql
@@ -0,0 +1,32 @@
+
+create database if not exists warframe;
+use warframe;
+
+create table if not exists parts(
+  name varchar(30) not null unique,
+  primary key (name),
+  ducats decimal(2) not null,
+  platinum enum('low','med','high') not null
+);
+
+create table if not exists endless(
+  ename varchar(30) not null,
+  foreign key (ename) references parts(name) on delete cascade,
+  tier enum('1', '2', '3', '4', 'derelict') not null,
+  type enum('survival', 'defense', 'interception') not null,
+  rotation enum('A', 'B', 'C') not null,
+  primary key (ename, tier, type, rotation),
+  chance decimal(3,1) not null,
+  rarity enum('common', 'uncommon', 'rare', 'legendary') not null
+);
+
+create table if not exists not_endless(
+  nename varchar(30) not null,
+  foreign key (nename) references parts(name) on delete cascade,
+  tier enum('1', '2', '3', '4') not null,
+  type enum('capture', 'exterminate', 'sabotage', 'mobile defense') not null,
+  primary key (nename, tier, type),
+  chance decimal(3,1) not null,
+  rarity enum('common', 'uncommon', 'rare', 'legendary') not null
+);
+  

e26e93f9262664e45c76330e72c686983d1ac198
bstephen1 <bstep1@umbc.edu>
2016-05-30 18:04:51 -0400

    added exterminate and survival towers

--- sql_data.sql
+++ sql_data.sql
@@ -3,3 +3,13 @@ insert into parts values('ash blueprint', 50, 'high');
 insert into parts values('ash chassis', 20, 'low');
 insert into parts values('ash helmet', 30, 'low');
 insert into parts values('ash systems', 50, 'high');
+
+
+insert into towers values('I', 'exterminate', 0);
+insert into towers values('II', 'exterminate', 0);
+insert into towers values('III', 'exterminate', 0);
+insert into towers values('IV', 'exterminate', 0);
+insert into towers values('I', 'survival', 1);
+insert into towers values('II', 'survival', 1);
+insert into towers values('III', 'survival', 1);
+insert into towers values('IV', 'survival', 1);

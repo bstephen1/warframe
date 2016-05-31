479018e5974af119069cbbbb9bb7324411fa97d4
bstephen1 <bstep1@umbc.edu>
2016-05-31 19:15:18 -0400

    added all prime parts to all towers
    switched insertion format to tower instead of prime set

--- sql_data.sql
+++ sql_data.sql
@@ -34,6 +34,94 @@ insert into endless values('braton stock', 'II', 'defense', 'C', 20, 'C');
 insert into endless values('braton stock', 'III', 'defense', 'C', 19.4, 'C');
 
 
+insert into parts values('akbronco blueprint', 10, 'low');
+insert into parts values('akbronco link', 30, 'low');
+
+insert into parts values('ankyros blueprint', 20, 'low');
+insert into parts values('ankyros blueprint', 50, 'high');
+insert into parts values('ankyros blueprint', 20, 'low');
+
+insert into parts values('bo blueprint', 20, 'low');
+insert into parts values('bo handle', 30, 'high');
+insert into parts values('bo ornament', 10, 'low');
+
+insert into parts values('boar blueprint', 20, 'high');
+insert into parts values('boar barrel', 20, 'high');
+insert into parts values('boar receiver', 10, 'low');
+insert into parts values('boar stock', 20, 'med');
+
+insert into parts values('boltor blueprint', 30, 'high');
+insert into parts values('boltor barrel', 20, 'low');
+insert into parts values('boltor receiver', 20, 'high');
+insert into parts values('boltor stock', 10, 'low');
+
+insert into parts values('bronco blueprint', 10, 'low');
+insert into parts values('bronco barrel', 50, 'low');
+insert into parts values('bronco receiver', 20, 'low');
+
+insert into parts values('burston blueprint', 20, 'low');
+insert into parts values('burston barrel', 20, 'low');
+insert into parts values('burston receiver', 30, 'low');
+insert into parts values('burston stock', 10, 'low');
+
+insert into parts values('carrier blueprint', 20, 'low');
+insert into parts values('carrier carapace', 50, 'med');
+insert into parts values('carrier cerebrum', 50, 'high');
+insert into parts values('carrier systems', 30, 'low');
+
+insert into parts values('dakra blueprint', 20, 'high');
+insert into parts values('dakra blade', 20, 'high');
+insert into parts values('dakra handle', 10, 'high');
+
+insert into parts values('dual kamas blueprint', 20, 'low');
+insert into parts values('dakra blade', 50, 'med');
+insert into parts values('dakra handle', 20, 'high');
+
+insert into parts values('ember blueprint', 50, 'high');
+insert into parts values('ember chassis', 10, 'high');
+insert into parts values('ember helmet', 10, 'low');
+insert into parts values('ember systems', 30, 'high');
+
+insert into parts values('fang blueprint', 20, 'low');
+insert into parts values('fang blade', 10, 'low');
+insert into parts values('fang handle', 20, 'med');
+
+insert into parts values('frost blueprint', 20, 'high');
+insert into parts values('frost chassis', 10, 'high');
+insert into parts values('frost helmet', 10, 'high');
+insert into parts values('frost systems', 20, 'high');
+
+insert into parts values('glaive blueprint', 50, 'high');
+insert into parts values('glaive blade', 20, 'high');
+insert into parts values('glaive disc', 20, 'med');
+
+insert into parts values('hikou blueprint', 10, 'low');
+insert into parts values('hikou pouch', 20, 'low');
+insert into parts values('hikou stars', 20, 'low');
+
+insert into parts values('kavasa blueprint', 30, 'low');
+insert into parts values('kavasa band', 50, 'low');
+insert into parts values('kavasa buckle', 50, 'high');
+
+insert into parts values('latron blueprint', 10, 'high');
+insert into parts values('latron barrel', 10, 'high');
+insert into parts values('latron receiver', 10, 'med');
+insert into parts values('latron stock', 10, 'low');
+
+insert into parts values('lex blueprint', 10, 'low');
+insert into parts values('lex barrel', 20, 'low');
+insert into parts values('lex receiver', 30, 'low');
+
+insert into parts values('loki blueprint', 30, 'low');
+insert into parts values('loki chassis', 20, 'high');
+insert into parts values('loki helmet', 30, 'low');
+insert into parts values('loki systems', 50, 'high');
+
+insert into parts values('mag blueprint', 10, 'med');
+insert into parts values('mag blueprint', 10, 'med');
+insert into parts values('mag blueprint', 10, 'med');
+
+
 insert into towers values('I', 'exterminate', 0);
 insert into towers values('II', 'exterminate', 0);
 insert into towers values('III', 'exterminate', 0);

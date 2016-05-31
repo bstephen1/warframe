a2910a25e897179221bb8b6df1bc404a2e55f8c7
bstephen1 <bstep1@umbc.edu>
2016-05-31 14:36:29 -0400

    added drop percentage to ash drops

--- sql_data.sql
+++ sql_data.sql
@@ -3,11 +3,11 @@ insert into parts values('ash blueprint', 50, 'high');
 insert into parts values('ash chassis', 20, 'low');
 insert into parts values('ash helmet', 30, 'low');
 insert into parts values('ash systems', 50, 'high');
-insert into endless values('ash blueprint', 'III', 'defense', 'C', 'U');
-insert into endless values('ash chassis', 'II', 'survival', 'C', 'C');
-insert into endless values('ash chassis', 'IV', 'survival', 'A', 'C');
-insert into endless values('ash systems', 'III', 'survival', 'C', 'U');
-insert into not_endless values('ash helmet', 'IV', 'sabotage', 'C');
+insert into endless values('ash blueprint', 'III', 'defense', 'C', 7.5, 'U');
+insert into endless values('ash chassis', 'II', 'survival', 'C', 19.4, 'C');
+insert into endless values('ash chassis', 'IV', 'survival', 'A', 25, 'C');
+insert into endless values('ash systems', 'III', 'survival', 'C', 5.5, 'U');
+insert into not_endless values('ash helmet', 'IV', 'sabotage', 15.5, 'C');
 
 
 insert into towers values('I', 'exterminate', 0);

8335430bd320415a5cbaea9612dd56ad1998530c
bstephen1 <bstep1@umbc.edu>
2016-05-31 15:49:53 -0400

    removed old code

--- main.php
+++ main.php
@@ -137,32 +137,5 @@
 	<p> get the ducats search to show best ducats </p>
 	<p> fill the database </p>
 	
-	<?php
-	/*
-		//connect to the prime part database
-		$servername = 'localhost';
-		$username = 'root';
-		$password = 'secret';
-		$db = 'warframe';
-		
-		//create connection
-		$con = new mysqli($servername, $username, $password, $db);
-		
-		// Check connection
-		if ($con->connect_error) {
-			die("Connection failed: " . $con->connect_error);
-		}
-		echo "Connected successfully<br>"; 
-		
-		//select all parts
-		$sql = "select * from parts";
-		$result = $con->query($sql);
-		for($i = 0; $i < $result->num_rows; $i++) {
-			$row = $result->fetch_assoc();
-			echo "name: " . $row["name"] . " ducats: " . $row["ducats"] . " plat: " . $row["platinum"] . "<br>";
-		}
-	*/
-	?>
-	
  </body>
 </html>
